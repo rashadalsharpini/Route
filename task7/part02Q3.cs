634af7bcd9615930a91c02d5ca6cdb284b6f3a20
rashad <rashadalsharpini@gmail.com>
2025-01-20 16:44:35 +0200

    part02Q3.cs

---
+++ task7/part02Q3.cs
@@ -0,0 +1,38 @@
+﻿INotificationService notify1 = new EmailService();
+INotificationService notify2 = new SmsService();
+INotificationService notify3 = new PushService();
+notify1.SendNotification("user@example.com", "Hello, this is an email notification!");
+notify2.SendNotification("1234567890", "Hello, this is an SMS notification!");
+notify3.SendNotification("device123", "Hello, this is a push notification!");
+
+public interface INotificationService
+{
+    void SendNotification(string recipient, string message);
+}
+
+public class EmailService : INotificationService
+{
+    public void SendNotification(string recipient, string message)
+    {
+        Console.WriteLine("Sending notification to " + recipient);
+        Console.WriteLine($"message{message}");
+    }
+}
+
+public class SmsService : INotificationService
+{
+    public void SendNotification(string recipient, string message)
+    {
+        Console.WriteLine("Sending notification to " + recipient);
+        Console.WriteLine($"message{message}");
+    }
+}
+
+public class PushService : INotificationService
+{
+    public void SendNotification(string recipient, string message)
+    {
+        Console.WriteLine("Sending notification to " + recipient);
+        Console.WriteLine($"message{message}");
+    }
+}
