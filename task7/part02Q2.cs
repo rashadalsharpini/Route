5c85bb1245d65064db4a084fc6a40f0bb6dd7571
rashad <rashadalsharpini@gmail.com>
2025-01-24 20:28:39 +0200

    update

--- task7/part02Q2.cs
+++ task7/part02Q2.cs
@@ -8,18 +8,33 @@ public interface IAuthenticationService
     public bool AuthorizeUser(string username, string password);
 }
 
+public class authorizedUser
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
 public class BasicAuthenticationService : IAuthenticationService
 {
-    private string username = "admin";
-    private string password = "admin";
+    private authorizedUser[] users = new authorizedUser[3];
+    public BasicAuthenticationService()
+    {
+        users[0] = new authorizedUser { Username = "admin", Password = "admin" };
+        users[1] = new authorizedUser { Username = "user2", Password = "pass2" };
+        users[2] = new authorizedUser { Username = "user3", Password = "pass3" };
+    }
     public bool AuthenticateUser(string username, string password)
     {
-        return username == this.username && password == this.password;  
+        foreach (authorizedUser user in users)
+        {
+            if (user.Username == username && user.Password == password)
+                return true;
+        } 
+        return false;
     }
 
     public bool AuthorizeUser(string username, string password)
     {
-        return username == this.username && password == this.password;
+        return AuthenticateUser(username, password);
     }
 }
-// to be honest i didn't understand this question 
+
